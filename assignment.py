1846e4087d4f5e63143e0a34bb603fbc33bba480
EthanWhite1122 <ethan.white1@stu.ocsb.ca>
2024-11-05 19:40:48 +0000

    fixed syntax

--- assignment.py
+++ assignment.py
@@ -5,22 +5,22 @@ while ( redo ):
   x = input("Enter Size Large or Extra Large:")
   if x == "Large" or x == "L" or x == "large" or x == "l":
     a=6
-  elif or x == "Extra Large" or x == "extra large" or x == "Extra large" or x == "extra Large" or x == "XL" or x == "Xl" or x == "xl" or x == "xL":
+  elif x == "Extra Large" or x == "extra large" or x == "Extra large" or x == "extra Large" or x == "XL" or x == "Xl" or x == "xl" or x == "xL":
     a=10
   else:
-    print"invalid size"
+    print("invalid size")
 
-while (redo) 
+while (redo): 
   y = input("Enter Amount of Toppings")
-  if y = 1:
-    a = float(a) + 1
-  elif y = 2:
-    a = float(a) + 1.75
-  elif y = 3:
-    a = float(a) + 2.50
-  elif y = 4:
-    a = float(a) + 3.35
+  if y == 1:
+    a == float(a) + 1
+  elif y == 2:
+    a == float(a) + 1.75
+  elif y == 3:
+    a == float(a) + 2.50
+  elif y == 4:
+    a == float(a) + 3.35
   else: 
-    print"invalid size"
-a = a*hst
-print"Your cost is" + str(a)
+    print("invalid size")
+a = a*1.13
+print("Your cost is" + str(a))
