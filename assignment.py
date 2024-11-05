72510a8324570106c3a48746b8396bec2509dad2
EthanWhite1122 <ethan.white1@stu.ocsb.ca>
2024-11-05 15:49:58 +0000

    start of 3b

--- assignment.py
+++ assignment.py
@@ -1,1 +1,26 @@
+import math
+redo = True
+a=0
+while ( redo ):
+  x = input("Enter Size Large or Extra Large:")
+  if x == "Large" or x == "L" or x == "large" or x == "l":
+    a=6
+  elif or x == "Extra Large" or x == "extra large" or x == "Extra large" or x == "extra Large" or x == "XL" or x == "Xl" or x == "xl" or x == "xL":
+    a=10
+  else:
+    print"invalid size"
+
+while (redo) 
+  y = input("Enter Amount of Toppings")
+  if y = 1:
+    a = float(a) + 1
+  elif y = 2:
+    a = float(a) + 1.75
+  elif y = 3:
+    a = float(a) + 2.50
+  elif y = 4:
+    a = float(a) + 3.35
+  else: 
+    print"invalid size"
+cost=((size+toppings)*hst)
 
